aa0c4cb9f85f287c5ad90933e84d0534e39633bf
Andrey Matveev <ra1ze.matveev@yandex.ru>
2025-05-01 18:00:25 +0300

    refactoring

--- src/handlers/rate_test.go
+++ src/handlers/rate_test.go
@@ -4,44 +4,20 @@ import (
 	"testing"
 
 	"github.com/Ra1ze505/goNewsBot/src/keyboard"
+	mock_repository "github.com/Ra1ze505/goNewsBot/src/mocks/repository"
 	mock_telebot "github.com/Ra1ze505/goNewsBot/src/mocks/telebot"
 	"github.com/Ra1ze505/goNewsBot/src/repository"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"go.uber.org/mock/gomock"
 )
 
-type MockRateRepository struct {
-	mock.Mock
-}
-
-func (m *MockRateRepository) SaveRate(rate *repository.Rate) error {
-	args := m.Called(rate)
-	return args.Error(0)
-}
-
-func (m *MockRateRepository) GetLatestRate() (*repository.Rate, error) {
-	args := m.Called()
-	return args.Get(0).(*repository.Rate), args.Error(1)
-}
-
-func (m *MockRateRepository) GetRates() (*repository.Rates, error) {
-	args := m.Called()
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*repository.Rates), args.Error(1)
-}
-
 func TestRateHandler_Handle(t *testing.T) {
-	mockRepo := new(MockRateRepository)
-
 	// Test case: successful rate retrieval
 	t.Run("successful rate retrieval", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
-		mockRepo.ExpectedCalls = nil
+		mockRepo := mock_repository.NewMockRateRepositoryInterface(ctrl)
 		mockContext := mock_telebot.NewMockContext(ctrl)
 		handler := NewRateHandler(mockRepo)
 
@@ -60,12 +36,11 @@ func TestRateHandler_Handle(t *testing.T) {
 			"Доллар: 90.00 ₽ (изменение: 1.12%)\n" +
 			"Евро: 100.00 ₽ (изменение: 1.01%)"
 
-		mockRepo.On("GetRates").Return(rates, nil)
+		mockRepo.EXPECT().GetRates().Return(rates, nil)
 		mockContext.EXPECT().Send(expectedMessage, keyboard.GetStartKeyboard()).Return(nil)
 
 		err := handler.Handle(mockContext)
 		assert.NoError(t, err)
-		mockRepo.AssertExpectations(t)
 	})
 
 	// Test case: error retrieving rates
@@ -73,16 +48,15 @@ func TestRateHandler_Handle(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
-		mockRepo.ExpectedCalls = nil
+		mockRepo := mock_repository.NewMockRateRepositoryInterface(ctrl)
 		mockContext := mock_telebot.NewMockContext(ctrl)
 		handler := NewRateHandler(mockRepo)
 
 		errorMessage := "Извините, не удалось получить текущий курс валют. Попробуйте позже."
-		mockRepo.On("GetRates").Return(nil, assert.AnError)
+		mockRepo.EXPECT().GetRates().Return(nil, assert.AnError)
 		mockContext.EXPECT().Send(errorMessage, keyboard.GetStartKeyboard()).Return(nil)
 
 		err := handler.Handle(mockContext)
 		assert.NoError(t, err)
-		mockRepo.AssertExpectations(t)
 	})
 }
